920ccc881bd79ed22dc914f7d3c75bb2d00ae335
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-08-19 20:19:27 +0100

    Specs and change behaviour to remove whitespace based on text lines rather than blank lines
    Signed-off-by: Caius Durling <dev@caius.name>

--- lib/lstrip_on_steroids.rb
+++ lib/lstrip_on_steroids.rb
@@ -1,17 +1,10 @@
 module LStripOnSteroids
   def -@
-    # Split into lines so we can process them
-    lines = self.split("\n")
-    # Lose the first line (it's empty anyway)
-    lines.delete_at(0)
-    # Grab the last line, which should just contain spaces
-    leading_whitespace = lines.delete_at(-1)
-    # Strip leading_whitespace from each line and join into one again
-    lines.map do |l|
-      i = leading_whitespace.length
-      l[/^ {#{i}}/] = "" if l[/^ {#{i}}/]
-      l
-    end.join("\n")
+    lines = split("\n")[1...-1]
+    leading_whitespace = lines.map { |line| line.match(/^ */)[0].length }.min
+    lines.map { |line|
+      line.sub(/^ {#{leading_whitespace}}/, "")
+    }.join("\n")
   end
 end
 
---
+++ spec/lstrip_on_steroids_spec.rb
@@ -0,0 +1,37 @@
+require File.join(File.dirname(__FILE__), "..", "lib", "lstrip_on_steroids")
+
+describe String do
+  describe "#-@" do
+    it "removes whitespace" do
+      string = -%{
+        foo
+          bar
+        baz
+      }
+      
+      string.should == "foo\n  bar\nbaz"
+    end
+
+    it "handles indented first lines" do
+      string = -%{
+          foo
+        bar
+          baz
+      }
+      
+      string.should == "  foo\nbar\n  baz"
+    end
+    
+    it "handles lines with really long indents" do
+      # In case we do the space removal naively
+      
+      string = -%{
+        foo
+                  bar
+        baz
+      }
+      
+      string.should == "foo\n          bar\nbaz"  
+    end
+  end
+end
