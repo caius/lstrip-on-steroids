2ce4c63e8e5c3584713d884df9afc08ca075f0d6
Caius Durling <dev@caius.name>
2010-08-20 20:28:31 +0100

    Code style tweaks

--- lib/lstrip_on_steroids.rb
+++ lib/lstrip_on_steroids.rb
@@ -1,32 +1,30 @@
 module LStripOnSteroids
   class JuicedStripper
-    def initialize(lines)
+    def initialize lines
       @lines = lines
       strip_bare_top_and_bottom
     end
     
     def strip
-      @lines.map { |line|
-        line.sub(/^ {#{whitespace_trim_length}}/, "")
-      }
+      @lines.map {|line| line.sub(/^ {#{whitespace_trim_length}}/, "") }
     end
     
     private
     
     def whitespace_trim_length
-      @whitespace_trim_length ||= @lines.map { |line| indent_size(line) }.min
+      @whitespace_trim_length ||= @lines.map {|line| indent_size(line) }.min
     end
     
     def strip_bare_top_and_bottom
-      [0, -1].each { |i| @lines.delete_at(i) if bare?(@lines[i]) }
+      [0, -1].each {|i| @lines.delete_at(i) if bare?(@lines[i]) }
     end
     
-    def indent_size(line)
+    def indent_size line
       line.match(/^ */)[0].length
     end
     
-    def bare?(line)
-      line =~ /^ *$/
+    def bare? line
+      line[/^ *$/]
     end
   end
   
